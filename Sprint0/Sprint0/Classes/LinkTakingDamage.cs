7fb854e3fb0a0076dc958abe4b8e079e82506da7
Yura <pozdneev1@gmail.com>
2023-02-20 10:56:47 -0500

    Flashing damage
    Still need to fix derection facing  when damaged

--- Sprint0/Sprint0/Classes/LinkTakingDamage.cs
+++ Sprint0/Sprint0/Classes/LinkTakingDamage.cs
@@ -13,22 +13,51 @@ namespace Sprint0
     {
         public Texture2D Texture { get; set; }
 
+        private int currentFrame;
+        private int totalFrames;
+
         public LinkTakingDamage(Texture2D texture)
         {
             Texture = texture;
-
+            currentFrame = 0;
+            totalFrames = 6;
         }
         public void Draw(SpriteBatch spriteBatch, Vector2 location)
         {
-            Rectangle sourceRectangle = new Rectangle(35, 232, 16, 16);
-            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, 40, 40);
+
+            Rectangle sourceRectangle = new Rectangle(0, 0, 0, 0);
+            Rectangle destinationRectangle = new Rectangle(0, 0, 0, 0);
+
+            if (currentFrame <= 2)
+            {
+                //animates one frame
+                sourceRectangle = new Rectangle(35, 232, 16, 16);
+                destinationRectangle = new Rectangle((int)location.X, (int)location.Y, 40, 40);
+            }
+            else if (currentFrame <= 4)
+            {
+                //animates one frame
+                sourceRectangle = new Rectangle(18, 232, 16, 16);
+                destinationRectangle = new Rectangle((int)location.X, (int)location.Y, 40, 40);
+            }
+            else if (currentFrame > 4)
+            {
+                //animates one frame
+                sourceRectangle = new Rectangle(1, 232, 16, 16);
+                destinationRectangle = new Rectangle((int)location.X, (int)location.Y, 40, 40);
+            }
 
             spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
         }
 
         public void Update()
         {
-            //nothing to do here
+            currentFrame++;
+
+            if (currentFrame == totalFrames)
+            {
+                currentFrame = 0;
+            }
         }
     }
 }
