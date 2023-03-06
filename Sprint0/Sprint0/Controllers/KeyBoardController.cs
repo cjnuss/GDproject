a5cc55a42e11f3580726687ce27b59412d2711c6
jacklukich <119974245+jacklukich@users.noreply.github.com>
2023-03-06 13:19:15 -0500

    Organized files and made our solution explorer much nicer

---
+++ Sprint0/Sprint0/Controllers/KeyBoardController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using System.Security.Cryptography;
+using System.Runtime.CompilerServices;
+using System.ComponentModel.Design;
+using Sprint0.Link_Classes;
+
+namespace Sprint0
+{
+    public class KeyBoardController : IController
+    {
+        private Game1 game1;
+        public Link linkSprite;
+
+        public Texture2D Texture { get; set; }
+        public SpriteBatch _spriteBatch;
+
+        public int dir, linkState;
+        public Vector2 location;
+
+        public MapCommands mappingCommands;
+        public Dictionary<Keys, ICommand> controllerMapping;
+
+        public KeyBoardController(Game1 game1, SpriteBatch spriteBatch)
+        {
+            this.game1 = game1;
+
+            controllerMapping = new Dictionary<Keys, ICommand>();
+            mappingCommands = new MapCommands(this, controllerMapping, game1, linkSprite);
+            mappingCommands.createCommands();
+            controllerMapping = mappingCommands.getControllerMapping(controllerMapping);
+
+            linkSprite = new Link(game1);
+
+            dir = 0;
+            linkState = 0;//mine
+            _spriteBatch = spriteBatch;
+        }
+        public void Update()
+        {
+            linkState = 0;
+
+            Keys[] pressedKeys = Keyboard.GetState().GetPressedKeys();
+
+            foreach (Keys key in pressedKeys)
+            {
+                if (controllerMapping.ContainsKey(key))
+                    controllerMapping[key].Execute();
+            }
+
+            linkSprite.Update(linkState, dir, location);
+            linkSprite.Draw(_spriteBatch);
+        }
+
+    }
+}
