8d2b458e68244b4c260a9978a1cb3cf22c11251e
jacklukich <119974245+jacklukich@users.noreply.github.com>
2023-04-03 13:33:25 -0400

    Added more magic number dealings

--- Sprint0/Sprint0/Constants/LinkConstants.cs
+++ Sprint0/Sprint0/Constants/LinkConstants.cs
@@ -61,5 +61,32 @@ namespace Sprint0
         // link moving
         public const int TotalMovingFrames = 20;
         public const int MovingPhase = TotalMovingFrames / 2;
+        // blue arrow
+        public const int TotalBlueArrowFrames = 30;
+        public const int BlueArrowMultiplier = 32;
+        public const int BlueArrowPosChange = 7;
+        // green arrow
+        public const int TotalGreenArrowFrames = 30;
+        public const int GreenArrowMultiplier = 16;
+        public const int GreenArrowPosChange = 5;
+        // bomb
+        public const int BombTotalFrames = 60;
+        public const int xOffset1 = 10;
+        public const int xOffset2 = 32;
+        public const int xOffset3 = 48;
+        public const int yOffset1 = 48;
+        public const int yOffset2 = 10;
+        public const int BombPhase1 = 36;
+        public const int BombPhase2 = 44;
+        public const int BombPhase3 = 52;
+        public const int BombPhase4 = 59;
+        // fire
+        public const int FireTotalFrames = 10;
+        public const int FireMultiplier = 24;
+        public const int FirePosChange = 3;
+        public const int FirePhase = FireTotalFrames / 2;
+        public const int StillCount = 20;
+        // sword beam
+
     }
 }
