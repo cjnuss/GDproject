ce9ed64b254d18006a821ffc5797432b4d484abd
jacklukich <119974245+jacklukich@users.noreply.github.com>
2023-04-03 23:32:06 -0400

    Implemented Constants for all Link Classes

--- Sprint0/Sprint0/Constants/LinkConstants.cs
+++ Sprint0/Sprint0/Constants/LinkConstants.cs
@@ -87,6 +87,19 @@ namespace Sprint0
         public const int FirePhase = FireTotalFrames / 2;
         public const int StillCount = 20;
         // sword beam
-
+        public const int SwordBeamTotalFrames = 10;
+        public const int SwordBeamPosChange = 7;
+        public const int SwordBeamMultiplier = 32;
+        public const int SwordBeamPhase = (SwordBeamTotalFrames / 2) + 1;
+        public const int SwordBeamXOffset1 = 15;
+        public const int SwordBeamXOffset2 = -18;
+        public const int SwordBeamXOffset3 = 18;
+        public const int SwordBeamXOffset4 = 9;
+        public const int SwordBeamYOffset1 = 18;
+        public const int SwordBeamYOffset2 = 17;
+        public const int SwordBeamYOffset3 = 16;
+        public const int SwordBeamYOffset4 = -19;
+        public const int SwordBeamOffsetIncrease = 2;
+        public const int SwordBeamCount = 30;
     }
 }
